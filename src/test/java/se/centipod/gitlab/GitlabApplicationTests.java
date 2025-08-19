c7f0e8794c2acf0ab279c9bcd49882c870b1b41c
Centipod-se <mats@centipod.se>
2025-08-19 13:32:27 +0000

    First commit

---
+++ src/test/java/se/centipod/gitlab/GitlabApplicationTests.java
@@ -0,0 +1,13 @@
+package se.centipod.gitlab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitlabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
